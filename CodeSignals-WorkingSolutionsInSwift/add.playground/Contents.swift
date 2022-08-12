f46ce210f7bd3c1dcdf3756a276422b90212af48
Roy <95645767+RoyNkem@users.noreply.github.com>
2022-08-12 13:50:43 +0100

    Solved 01 - Add

---
+++ CodeSignals-WorkingSolutionsInSwift/add.playground/Contents.swift
@@ -0,0 +1,13 @@
+/*
+ Write a function that returns the sum of two numbers.
+
+Example:
+For param1 = 1 and param2 = 2, the output should be add(param1, param2) = 3.
+ */
+
+func add(param1: Int, param2: Int) -> Int {
+    return param1 + param2
+}
+
+//Test
+add(param1: 20, param2: 45)
