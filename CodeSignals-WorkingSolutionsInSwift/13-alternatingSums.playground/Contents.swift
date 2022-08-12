0378e08e8caf4d405de01c35720b6a83b057066e
Roy <95645767+RoyNkem@users.noreply.github.com>
2022-08-12 23:56:45 +0100

    Solve 13-alternatingSums

---
+++ CodeSignals-WorkingSolutionsInSwift/13-alternatingSums.playground/Contents.swift
@@ -0,0 +1,28 @@
+/*
+ Several people are standing in a row and need to be divided into two teams. The first person goes into team 1,
+ the second goes into team 2, the third goes into team 1 again, the fourth into team 2, and so on.
+ 
+ You are given an array of positive integers - the weights of the people. Return an array of two integers,
+ where the first element is the total weight of team 1, and the second element is the total weight
+ of team 2 after the division is complete.
+ 
+ Example:
+ - For a = [50, 60, 60, 45, 70], the output should be alternatingSums(a) = [180, 105].
+ */
+
+func solution(a: [Int]) -> [Int] {
+    var alternatingSums = [0, 0]
+    
+    for i in (0..<a.count) {
+        if i % 2 == 0 {
+            alternatingSums[0] += a[i]
+        } else {
+            alternatingSums[1] += a[i]
+        }
+    }
+    return alternatingSums
+}
+
+//Test
+let a = [50, 60, 60, 45, 70]
+solution(a: a)
