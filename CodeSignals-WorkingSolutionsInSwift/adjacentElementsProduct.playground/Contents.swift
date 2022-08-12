dac9a404f54b309f748f491441868b007051f830
Roy <95645767+RoyNkem@users.noreply.github.com>
2022-08-12 16:24:29 +0100

    Solve 04 - adjacentElementsProduct

--- CodeSignals-WorkingSolutionsInSwift/adjacentElementsProduct.playground/Contents.swift
+++ CodeSignals-WorkingSolutionsInSwift/adjacentElementsProduct.playground/Contents.swift
@@ -5,27 +5,21 @@
 //  Created by Roy Aiyetin on 12/08/2022.
 
 /*Given an array of integers, find the pair of adjacent elements that has the largest product and return that product.
-
-Example:
-For inputArray = [3, 6, -2, -5, 7, 3], the output should be adjacentElementsProduct(inputArray) = 21.
-7 and 3 produce the largest product.
+ 
+ Example:
+ For inputArray = [3, 6, -2, -5, 7, 3], the output should be adjacentElementsProduct(inputArray) = 21.
+ 7 and 3 produce the largest product.
  */
 
 func solution(inputArray: [Int]) -> Int  {
-    var productArray: [Int] = []
-    var product = 0
+    var maxProduct = inputArray[0] * inputArray[1]
     
-    guard inputArray.count > 1 else {
-        return inputArray.first ?? 0
-    }
-
-    for i in (0..<inputArray.count) {
-        for j in (i+1..<inputArray.count) {
-            product = inputArray[i] * inputArray[j]
-            productArray.append(product)
+    for i in (0..<inputArray.count-1) {
+        if inputArray[i] * inputArray[i+1] > maxProduct {
+            maxProduct = inputArray[i] * inputArray[i+1]
         }
     }
-    return productArray.max()!
+    return maxProduct
 }
 
 //Test
