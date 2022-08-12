a5111630037700d8ed56a1f1b8da9012e30de91f
Roy <95645767+RoyNkem@users.noreply.github.com>
2022-08-12 22:16:27 +0100

    Solve 10-isLucky

---
+++ CodeSignals-WorkingSolutionsInSwift/10-isLucky.playground/Contents.swift
@@ -0,0 +1,38 @@
+/*
+ Ticket numbers usually consist of an even number of digits. A ticket number is considered lucky
+ if the sum of the first half of the digits is equal to the sum of the second half.
+ Given a ticket number n, determine if it's lucky or not.
+ 
+ Example
+ - For n = 1230, the output should be isLucky(n) = true;
+ - For n = 239017, the output should be isLucky(n) = false.
+ */
+
+func isLucky(n: Int) -> Bool {
+    guard String(n).count % 2 == 0 else {return false}
+    
+    var digit = n.digits
+    let mid = digit.count/2
+    let leftHalf = digit[0..<mid]
+    let rightHalf = digit[mid..<digit.count]
+    
+    let leftSum = leftHalf.reduce(0, +)
+    let rightSum = rightHalf.reduce(0, +)
+    
+    guard leftSum == rightSum else {
+        return false
+    }
+    
+    return true
+}
+
+extension BinaryInteger {
+    var digits: [Int] {
+        return String(describing: self).compactMap { Int(String($0)) }
+    }
+}
+
+//Test
+let n = 23901
+isLucky(n: n)
+
