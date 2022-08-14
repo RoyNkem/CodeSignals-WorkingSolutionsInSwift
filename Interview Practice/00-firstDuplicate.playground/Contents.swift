9de0377b937494285219a8216fff41ee7a2500da
Roy <95645767+RoyNkem@users.noreply.github.com>
2022-08-14 15:20:41 +0100

    Solve 00-firstduplicate

--- Interview Practice/00-firstDuplicate.playground/Contents.swift
+++ Interview Practice/00-firstDuplicate.playground/Contents.swift
@@ -1,9 +1,20 @@
 /*
- 
+ Given an array a that contains only numbers in the range from 1 to a.length, find the first duplicate number for which the second occurrence has the minimal index. In other words, if there are more than 1 duplicated numbers, return the number for which the second occurrence has a smaller index than the second occurrence of the other number does. If there are no such elements, return -1.
+
+ Example
+
+ For a = [2, 1, 3, 5, 3, 2], the output should be solution(a) = 3.
+
+ There are 2 duplicates: numbers 2 and 3. The second occurrence of 3 has a smaller index than the second occurrence of 2 does, so the answer is 3.
+
+ For a = [2, 2], the output should be solution(a) = 2;
+
+ For a = [2, 4, 3, 5, 1], the output should be solution(a) = -1.
  */
 
 func solution(a: [Int]) -> Int {
-    var set:Set<Int> = Set()
+    var set: Set<Int> = Set()
+    
     for i in a {
         if(set.contains(i)) {
             return i;
@@ -11,9 +22,10 @@ func solution(a: [Int]) -> Int {
             set.insert(i);
         }
     }
+    
     return -1;
 }
 
 //Test
-let a =
+let a = [2,3,3,2]
 solution(a: a)
